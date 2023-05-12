ddbf8889239c0ceb404cf530c4ee8906b68a1d7b
littleRabbit0220 <bluecoder0220@gmail.com>
2023-05-12 12:38:24 +0300

    change router

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import FirstView from '../views/FirstView.vue'
-import SecondView from '../views/SecondView.vue';
-import ThirdView from '../views/ThirdView.vue';
+import { Question_One, Question_Two, Question_Three, Question_Four } from '../views/question'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: ThirdView
+      component: Question_One
     }
   ]
 })
--- src/views/question/Two.vue
+++ src/views/question/Two.vue
@@ -1,5 +1,21 @@
 <template>
-    <div>
-        This is question 2 page.
+    <div class="flex flex-col justify-center h-screen items-center">
+        <div class="form-content form-content-sm">
+            <div class="text-main-content mb-4 w-full">Given their situation, what would you request the learner do next</div>
+            
+        </div>
     </div>
 </template>
+<script>
+import ToggleCard from '../../components/ToggleCard.vue';
+export default {
+    components: {
+        ToggleCard,
+    },
+    name: 'Second Question',
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/styles/main.scss";
+</style>
