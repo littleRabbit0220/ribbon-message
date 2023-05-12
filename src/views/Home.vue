c34f66ddca7799f7b8f8f7df16041bb8ad3cd9bd
littleRabbit0220 <bluecoder0220@gmail.com>
2023-05-12 18:35:06 +0300

    change router

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        home page
+    </div>
+</template>
+<script>
+    export default {
+        created() {
+            this.$router.push('/questions/1');
+        }
+    }
+</script>
